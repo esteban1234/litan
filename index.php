27bee00a3b7b932ac1f20c776d715645197d689e
esteban1234 <tva_libra@hotmail.com>
2017-10-25 08:49:25 -0500

    metas

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="es">
   <head>
     <meta charset="utf-8">
-    
+    <meta name="google-site-verification" content="5X8sDafmdOPTuysYUPqevnPGmOuP_URwnPhxqM1xuGQ" />
     <link rel="icon" type="image/png" href="img/favicon.png" />
     <title>CONSTRUCCIONES LITAN</title>
    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
